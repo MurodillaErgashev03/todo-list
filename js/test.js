5b0b7d0960e15da6ba75401d0a63bc3f7c4ff6a6
MurodillaErgashev03 <ergashevmurodilla27@gmail.com>
2023-02-01 18:31:26 +0500

    finish

---
+++ js/test.js
@@ -0,0 +1,20 @@
+let todos = [
+	{
+		id:0,
+		title: "Kitob o'qish",
+		completed: false,
+		date: '2003-07-31',
+	},
+	{
+		id:1,
+		title: 'yugurish',
+		completed: false,
+		date: '2023-01-5',
+	},
+	{
+		id:2,
+		title: 'uyga qaytish',
+		completed: true,
+		date: '2016-09-10',
+	},
+];
